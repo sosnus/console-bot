26c149146ef8c5222b4d467e95c66cc96ee63ab3
dgliwka <dawid.gliwka@gmail.com>
2017-11-13 19:09:43 +0100

    Uzupełniłem godziny pracy dziekanatu (#2)

--- console-bot/main-app/Program.cs
+++ console-bot/main-app/Program.cs
@@ -38,7 +38,13 @@ namespace main_app
                     message.Contains("<tutaj wpsiz inne slowo kluczowe dla godzin dziekanskich>") ||
                     message.Contains("<i jeszcze inne słowo kluczowe>"))
                 {
-                    Console.WriteLine("godziny pracy dziekanatu");
+                    Console.WriteLine("poniedziałek, wtorek, piątek: 10:00 - 13:00");
+                    Console.WriteLine("czwartek: 12:00 - 15:00:");
+                    Console.WriteLine("środa: nieczynne");
+                    Console.WriteLine("Okienko 1");
+                    Console.WriteLine("poniedziałek, wtorek, piątek: 10:00 - 15:00     przerwa 13:15 - 13:45");
+                    Console.WriteLine("czwartek: 11:00 - 16:00     przerwa 14:00 - 14:30");
+                    Console.WriteLine("środa: nieczynne");
                 }
                 else if (message.Contains("event") || message.Contains("wydarz"))
                 {
