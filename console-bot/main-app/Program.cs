c4cd488cbe12b19e0a44f760f489aaac85db8bde
sosnus <sosnus15@gmail.com>
2017-11-13 06:25:50 +0100

    first task

--- console-bot/main-app/Program.cs
+++ console-bot/main-app/Program.cs
@@ -8,8 +8,25 @@ namespace main_app
 {
     class Program
     {
+        static string message;
         static void Main(string[] args)
         {
+            message = Console.ReadLine();
+            message = message.ToLower();
+            if(message.Contains("dziekanat") || message.Contains("dean"))
+            {
+                /*zapytaj jaki wydział i wyświetl:
+                 *godziny otwarcia dzisiaj
+                 *czy aktualnie jest otwarte
+                 *ogólnie, kiedy dziekanat jest czynny
+                 * jak znaleźć?
+                 * dane kontaktowe (mail/tel) dziekanatu
+                 * strona www dziekanatu
+                 * dla WSZYSTKICH wydziałów
+                */
+            }
+
+
         }
     }
 }
