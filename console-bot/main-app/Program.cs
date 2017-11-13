b24fd89af65334206711def972970e482cf90b8b
sosnus <sosnus15@gmail.com>
2017-11-13 06:46:46 +0100

    other misc.

--- console-bot/main-app/Program.cs
+++ console-bot/main-app/Program.cs
@@ -11,22 +11,44 @@ namespace main_app
         static string message;
         static void Main(string[] args)
         {
-            message = Console.ReadLine();
-            message = message.ToLower();
-            if(message.Contains("dziekanat") || message.Contains("dean"))
+            while (true)
             {
-                /*zapytaj jaki wydział i wyświetl:
-                 *godziny otwarcia dzisiaj
-                 *czy aktualnie jest otwarte
-                 *ogólnie, kiedy dziekanat jest czynny
-                 * jak znaleźć?
-                 * dane kontaktowe (mail/tel) dziekanatu
-                 * strona www dziekanatu
-                 * dla WSZYSTKICH wydziałów
-                */
+                /*
+                 * inne protipy:
+                 * użyj daty: DateTime.Now;
+                 * najlepiej przekaż stringa message do nowej klasy
+                 * może wart zrobić wersję dwujęzyczną każdego modułu? Jak możemy to zrobić? ;)
+                 */
+                message = Console.ReadLine();
+                message = message.ToLower();
+                if (message.Contains("dziekanat") || message.Contains("dean"))
+                {
+                    //TODO: INFORMACJE O DZIEKANACIE
+                    /*zapytaj jaki wydział i wyświetl:
+                     * godziny otwarcia dzisiaj
+                     * czy aktualnie jest otwarte
+                     * ogólnie, kiedy dziekanat jest czynny
+                     * jak znaleźć?
+                     * dane kontaktowe (mail/tel) dziekanatu
+                     * strona www dziekanatu
+                     * dla WSZYSTKICH wydziałów
+                    */
+                }
+                
+                else if (message == "exit") break; //koniec działania aplikacji
+                else if (message == "co tu wpisać?") Info(); //TODO: Informacje o aplikacji
+                else       Help(); //TODO: wypisywanie komend
             }
+        }
 
+        private static void Info()
+        {
+            throw new NotImplementedException();
+        }
 
+        private static void Help()
+        {
+            throw new NotImplementedException();
         }
     }
 }
